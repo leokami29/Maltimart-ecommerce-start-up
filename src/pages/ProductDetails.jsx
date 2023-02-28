bce795f9605d787928255f04a07a382174e4c162
Leonel Polanco Fernandez <leonelpolanco2913@gmail.com>
2023-02-28 01:09:09 -0500

    2/27/2023
    Se creo Shop, prodocuto details con su respectivo css

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import { Col, Container, Row } from 'reactstrap'
+import { motion } from 'framer-motion'
+
+import products from '../assets/data/products'
+
+import CommonSection from '../components/UI/CommonSection'
+import Helmet from '../components/Helmet/Helmet'
+import '../Styles/products-datails.css'
 
 const ProductDetails = () => {
+
+  const {id} = useParams()
+  const product = products.find(item=> item.id === id)
+
+  const {
+          imgUrl,
+          productName, 
+          price, 
+          avgRating, 
+          review, 
+          description, 
+          shortDesc 
+        } = product
+
   return (
-    <div>ProductDetails</div>
+    <Helmet>
+      <CommonSection/>
+      <section className=' pt-0'>
+        <Container>
+          <Row>
+            <Col lang='6'>
+              <img src={imgUrl} alt="" />
+            </Col>
+            <Col lang='6'>
+              <div className="product__details">
+                <h2>{productName}</h2>
+                <div className='product__raiting d-flex align-content-center gap-5 mb-3'>
+                  <div>
+                    <span><i class="ri-star-s-fill"></i></span>
+                    <span><i class="ri-star-s-fill"></i></span>
+                    <span><i class="ri-star-s-fill"></i></span>
+                    <span><i class="ri-star-s-fill"></i></span>
+                    <span><i class="ri-star-half-fill"></i></span>
+                  </div>
+
+                  <p>(<span>{avgRating}</span>raitings)</p>
+                </div>
+
+                <span className='product__price'>${price}</span>
+                <p className=' mt-3'>{shortDesc}</p>
+                <motion.button whileTap={{scale: 1.3}} className='buy__btn'>Add to Cart</motion.button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,96 @@
-import React from 'react'
+import { useState } from 'react'
+import { Col, Container, Row } from 'reactstrap'
+
+import products from '../assets/data/products'
+
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection'
+import ProductsList from '../components/UI/ProductsList'
+
+import '../Styles/shop.css'
 
 const Shop = () => {
+
+  const [productsData, setProductsData] = useState(products)
+
+  const handleFilter = (e) => {
+    const filter = e.target.value
+    if (filter === 'sofa') {
+      const filterdProducts = products.filter(item => item.category==='sofa')
+      setProductsData(filterdProducts)
+    }
+    if (filter === 'mobile') {
+      const filterdProducts = products.filter(item => item.category==='mobile')
+      setProductsData(filterdProducts)
+    }
+    if (filter === 'chair') {
+      const filterdProducts = products.filter(item => item.category==='chair')
+      setProductsData(filterdProducts)
+    }
+    if (filter === 'watch') {
+      const filterdProducts = products.filter(item => item.category==='watch')
+      setProductsData(filterdProducts)
+    }
+    if (filter === 'wireless') {
+      const filterdProducts = products.filter(item => item.category==='wireless')
+      setProductsData(filterdProducts)
+    }
+  }
+
+  const handleSearch = e => {
+    const searchTerm = e.target.value
+
+    const searchedProducts =  products.filter(item => item.productName.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setProductsData(searchedProducts)
+  }
+
   return (
-    <div>Shop</div>
+    <Helmet title='Shop'>
+      <CommonSection title='Products'/>
+      <section className="">
+        <Container>
+          <Row>
+            <Col lg='3' md="3">
+              <div className="filter__widget">
+                <select onChange={handleFilter}>
+                  <option>Filter By Category</option>
+                  <option value="sofa">Sofa</option>
+                  <option value="mobile">Mobil</option>
+                  <option value="chair">Chair</option>
+                  <option value="watch">Watch</option>
+                  <option value="wireless">Wireless</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md="3"><div className="filter__widget">
+              <select>
+                <option>Sort By</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </div>
+            </Col>
+            <Col lg='6' md="6">
+              <div className="search__box">
+                <input type="text" placeholder='Search . . . .' onChange={handleSearch}/>
+                <span><i class="ri-search-line"></i> </span>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className='pt-0'>
+        <Container>
+          <Row>
+            {
+              productsData.length === 0? <h1 className=' text-center'>No Products are found!</h1>: <ProductsList data={productsData}/>
+            }
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
